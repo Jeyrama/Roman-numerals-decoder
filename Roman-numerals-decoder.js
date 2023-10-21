5b295994253e9eeaa200407f9fb87a164da285da
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-20 20:04:46 -0700

    Update Roman-numerals-decoder.js

--- Roman-numerals-decoder.js
+++ Roman-numerals-decoder.js
@@ -33,3 +33,20 @@ Help:
 
 
 // Solution
+
+function solution(roman) {
+	let data = {M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1};
+  let numbers = roman.split('');
+  let sum = 0, i;
+
+	for(i = 0; i < numbers.length; i++) {
+  	if(data[numbers[i]] < data[numbers[i+1]]) {
+    	sum += data[numbers[i+1]] - data[numbers[i]];
+      i++;
+    }
+    else {
+    	sum += data[numbers[i]];
+    }
+  }
+  return sum;
+}
