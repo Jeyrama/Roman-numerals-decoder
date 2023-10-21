24b8486b850003982572cee32af3d34322e08180
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-20 20:05:25 -0700

    Update Roman-numerals-decoder.js

--- Roman-numerals-decoder.js
+++ Roman-numerals-decoder.js
@@ -52,3 +52,8 @@ function solution(roman) {
 }
 
 // or
+
+function solution(roman) {
+  let value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
+  return roman.split('').map( d => value[d] ).reduce( (s,v,i,o) => s + ( (o[i+1] > v) ? -v : v ), 0 );
+}
